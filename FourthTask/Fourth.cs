00e88cbcdce35b94861b988d104258a6a3a37ec5
Viktor <dzubrovski@gmail.com>
2018-09-28 16:03:38 +0300

    added fourth task

---
+++ FourthTask/Fourth.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthTask
+{
+    class Fourth
+    {
+        static void Main(string[] args)
+        {
+
+            //input the word from the keyboard and convert it to array of chars
+            Console.WriteLine("Please, input the word: ");
+
+            string inputWord = Convert.ToString(Console.ReadLine());
+            char[] array = inputWord.ToCharArray();
+
+
+            if (IsPalindrom(array))
+
+                Console.WriteLine("YES - input word is a palindrom");
+          
+            else
+
+                Console.WriteLine("NO - input word isn't a palindrom");
+
+        }
+
+
+
+        static bool IsPalindrom(char[] array)
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+                if (array[i] != array[array.Length - 1 - i])
+                    return false;
+            return true;
+        }
+
+    }
+}
