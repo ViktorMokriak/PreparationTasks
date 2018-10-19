6a029b6cef8da26523d24324d7d9e6ba883e42b5
Viktor <dzubrovski@gmail.com>
2018-10-19 14:15:23 +0300

    Updated third task with try-catch construction

--- ThirdTask/Third.cs
+++ ThirdTask/Third.cs
@@ -16,41 +16,52 @@ namespace ThirdTask
             //entering the length of the array and checking whether it's longer than 10
             Console.WriteLine("Please, input the length of array (more than 10):");
 
-            arrLength = Convert.ToInt32(Console.ReadLine());
-            if (arrLength <= 10)
+            //blablabla
+
+            try
             {
-                Console.WriteLine("The input length should be more than 10.");
-                Console.ReadKey();
-            }
-            else
-            {
-                //initializing the array from keyboard input
-                int[] array = new int[arrLength];
-                int i = 0;
-                while (i < arrLength)
+                arrLength = Convert.ToInt32(Console.ReadLine());
+                if (arrLength <= 10)
                 {
-                    Console.WriteLine("Please, input element of array and hit Enter");
-                    array[i] = int.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    i++;
+                    Console.WriteLine("The input length should be more than 10.");
+                    Console.ReadKey();
                 }
+                else
+                {
+                    //initializing the array from keyboard input
+                    int[] array = new int[arrLength];
+                    int i = 0;
+                    while (i < arrLength)
+                    {
+                        Console.WriteLine("Please, input element of array and hit Enter");
+                        array[i] = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        i++;
+                    }
 
-                //printing the input array to console
-                for (i = 0; i < arrLength; i++)
-                Console.WriteLine("Element[" + i + "]: " + array[i]);
-                Console.ReadKey();
+                    //printing the input array to console
+                    for (i = 0; i < arrLength; i++)
+                        Console.WriteLine("Element[" + i + "]: " + array[i]);
+                    Console.ReadKey();
 
-                //check the main condition of the task and showing the result
-                for (i = 0; i < arrLength; i++)
+                    //check the main condition of the task and showing the result
+                    for (i = 0; i < arrLength; i++)
 
-                    if (array[i] % 3 == 0 && array[i] % 5 != 0)
-                       resultSum += array[i];
-                    
-                Console.WriteLine("The SUM of array elements that are multiple to 3 but not multiple to 5 is " + resultSum);
-                Console.ReadKey();
+                        if (array[i] % 3 == 0 && array[i] % 5 != 0)
+                            resultSum += array[i];
 
-            }   
-
+                    Console.WriteLine("The SUM of array elements that are multiple to 3 but not multiple to 5 is " + resultSum);
+                    Console.ReadKey();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("End of program.");
+            }
         }
     }
 }
