7dfe5bb07caaa1618226526e0d63d5f46392ecc7
Viktor <dzubrovski@gmail.com>
2018-10-31 18:00:22 +0200

    Completed Seventh Task

--- SeventhTask/Seventh.cs
+++ SeventhTask/Seventh.cs
@@ -11,42 +11,51 @@ namespace SeventhTask
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\vilto\Desktop\countries.txt";
+            string path = @"C:\Users\ViktorM\Desktop\countries.txt";
+            //string path = @"C:\Users\vilto\Desktop\countries.txt";
             Dictionary<int, Country> countries = new Dictionary<int, Country>();
+          
+            Console.WriteLine("-----List of European Union countries with Ukraine-----");
+            Console.WriteLine();
+               using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+               {
+                   string line;
+                   int temp=0;
+                   while ((line = sr.ReadLine()) != null)
+                   {
+                       Country country = new Country(line);
+                       countries.Add(temp, country);
+                       temp++;
+                   }
+               }
 
-            try
-            {
-                Console.WriteLine();
-                Console.WriteLine("-----All countries of European Union-----");
-                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
-                {
-                    string line;
-                    int temp=0;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        Country country = new Country(line);
+            //addind Ukraine to the dictionary
+            countries.Add(countries.Count + 1, new Country("Ukraine"));
 
-                        countries.Add(temp, country);
-                        temp++;
-                    }
-                }
-                
-                foreach (Country country in countries.Values)
-                {
-                    Console.WriteLine(country.nameOfCountry);
-                }
+               //print list of European Union countries with Ukraine
+               foreach (Country country in countries.Values)
+               {
+                   Console.WriteLine(country.nameOfCountry);
+               }
 
+            //print list of Countries where Telenor is supported
+            Console.WriteLine();
+            Console.WriteLine("-----List of European Union countries with Ukraine where Telenor is not supported-----");
+            Console.WriteLine();
+            foreach (Country country in countries.Values)
+               {
+                   if (country.nameOfCountry.Equals("Denmark") || country.nameOfCountry.Equals("Hungary")) 
+                   {
+                       country.isTelenorSupported = true;
+                   }
 
-            //    foreach (KeyValuePair<int, string> keyValue in countries)
-            //    {
-            //        Console.WriteLine(Country.);
-            //    }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+                   if (country.isTelenorSupported == false)
+                   {
+                       Console.WriteLine(country.nameOfCountry);
+                   }
+               }
+           
+            Console.ReadLine();
         }
     }
 }
